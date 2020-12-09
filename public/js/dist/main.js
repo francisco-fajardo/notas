86e7b226ae1c09cc55b96eeb8595cf8030f3866d
yonaikerlol <yonaikerlol@users.noreply.github.com>
2020-12-09 02:17:02 +0000

    Apply formatting changes

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -1,1 +1,13 @@
-(self.webpackChunknotas=self.webpackChunknotas||[]).push([[179],{265:function(){"use strict";document.addEventListener("DOMContentLoaded",(function(){return M.AutoInit()}))}},0,[[265,666]]]);
+(self.webpackChunknotas = self.webpackChunknotas || []).push([
+    [179],
+    {
+        265: function () {
+            "use strict";
+            document.addEventListener("DOMContentLoaded", function () {
+                return M.AutoInit();
+            });
+        },
+    },
+    0,
+    [[265, 666]],
+]);

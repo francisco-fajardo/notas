c5e7f615a879a6be2088769e3f49a5e1279fcabe
yonaikerlol <yonaikerlol@users.noreply.github.com>
2020-12-13 19:29:03 +0000

    Apply formatting changes

--- public/index.php
+++ public/index.php
@@ -90,7 +90,6 @@
 
       <ul class="collapsible">
 <?php foreach (new DirectoryIterator(__DIR__ . "/img/notas/") as $file) {
-
     if ($file->isDot()) {
         continue;
     }
@@ -103,8 +102,7 @@
     $section = $exploded[2];
 
     $courseName = $year . " " . $career . " &quot;" . $section . "&quot;";
-    $url = "img/notas/" . $filename;
-    ?>
+    $url = "img/notas/" . $filename; ?>
         <li>
           <div class="collapsible-header"><?php echo $courseName; ?></div>
           <div class="collapsible-body"><img data-src="<?php echo $url; ?>" alt="<?php echo "Notas del " .
